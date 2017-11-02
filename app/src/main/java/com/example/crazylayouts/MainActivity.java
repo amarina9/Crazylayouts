39d39282d2fa65ed1e7fd344ec495124202aa148
amarina9 <amarinadiaz@gmail.com>
2017-11-02 20:01:59 +0100

    second

--- app/src/main/java/com/example/crazylayouts/MainActivity.java
+++ app/src/main/java/com/example/crazylayouts/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.crazylayouts;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void A_Linear (View v){
+
+        Intent intent = new Intent(this, Linear1.class);
+        startActivity(intent);
+    }
+
+
+    public void A_Relative(View v){
+
+        Intent intent = new Intent(this, relative1.class);
+        startActivity(intent);
+
     }
 }
